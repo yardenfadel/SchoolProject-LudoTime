2cdfe7535b9fe9e7d02f5345094750fbda7c9bc8
yardenfadel <fadel.yarden@gmail.com>
2025-01-03 09:02:28 +0200

    added realtime database

--- app/src/main/java/com/example/ludotime/FireBaseController.java
+++ app/src/main/java/com/example/ludotime/FireBaseController.java
@@ -8,14 +8,28 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class FireBaseController {
     private static FirebaseAuth mAuth;
+    private static FirebaseDatabase database;
+    private static DatabaseReference ref;
 
     public static FirebaseAuth getAuth(){
         if (mAuth == null)
             mAuth = FirebaseAuth.getInstance();
         return mAuth;
+    }
+    public static FirebaseDatabase getDatabase(){
+        if (database == null)
+            database = FirebaseDatabase.getInstance();
+        return database;
+    }
+    public static DatabaseReference getReference(){
+        if (ref == null)
+            ref = getDatabase().getReference("Users");
+        return ref;
     }
 
 
@@ -26,6 +40,8 @@ public class FireBaseController {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             Log.d("TAG", "createUserWithEmail:success");
+                            getReference().child(task.getResult().getUser().getUid()).setValue(user);
+
                         } else {
                             Log.d("TAG", "createUserWithEmail:failure", task.getException());
                         }
